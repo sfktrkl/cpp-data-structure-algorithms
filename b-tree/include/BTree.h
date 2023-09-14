63f0b1f489c75b58339ea9ea39ffbe7f35d4d55a
Safak Turkeli <sfktrkl@gmail.com>
2023-09-14 05:51:44 +0300

    BTree insert

--- b-tree/include/BTree.h
+++ b-tree/include/BTree.h
@@ -33,12 +33,17 @@ namespace DataStructures
         }
 
         Node<T> *search(const T &key) const;
+        void insert(const T &key);
+        void print() const;
 
     private:
         int t;
         Node<T> *root;
 
         Node<T> *search(const T &key, Node<T> *node) const;
+        void split(Node<T> *x, int i);
+        void insert(Node<T> *x, const T &key);
+        void print(Node<T> *node, int depth = 0) const;
     };
 
     template <typename T>
@@ -60,5 +65,101 @@ namespace DataStructures
             return nullptr;
         else
             return search(key, node->children[i]);
+    }
+
+    template <typename T>
+    void BTree<T>::split(Node<T> *x, int i)
+    {
+        Node<T> *y = x->children[i];
+
+        Node<T> *z = new Node<T>(y->isLeaf);
+        x->children.insert(x->children.begin() + i + 1, z);
+
+        x->keys.insert(x->keys.begin() + i, y->keys[t - 1]);
+
+        z->keys = std::vector<T>(y->keys.begin() + t, y->keys.end());
+        y->keys = std::vector<T>(y->keys.begin(), y->keys.begin() + t - 1);
+
+        if (!y->isLeaf)
+        {
+            z->children = std::vector<Node<T> *>(y->children.begin() + t, y->children.end());
+            y->children = std::vector<Node<T> *>(y->children.begin(), y->children.begin() + t - 1);
+        }
+    }
+
+    template <typename T>
+    void BTree<T>::insert(const T &key)
+    {
+        Node<T> *root = this->root;
+        if (root->keys.size() == 2 * t - 1)
+        {
+            Node<T> *newRoot = new Node<T>();
+            this->root = newRoot;
+            newRoot->children.push_back(root);
+            split(newRoot, 0);
+            insert(newRoot, key);
+        }
+        else
+            insert(root, key);
+    }
+
+    template <typename T>
+    void BTree<T>::insert(Node<T> *x, const T &key)
+    {
+        int i = x->keys.size() - 1;
+
+        if (x->isLeaf)
+        {
+            x->keys.push_back(key);
+            while (i >= 0 && key < x->keys[i])
+            {
+                x->keys[i + 1] = x->keys[i];
+                --i;
+            }
+            x->keys[i + 1] = key;
+        }
+        else
+        {
+            while (i >= 0 && key < x->keys[i])
+            {
+                x->keys[i + 1] = x->keys[i];
+                --i;
+            }
+            ++i;
+            if (x->children[i]->keys.size() == 2 * t - 1)
+            {
+                split(x, i);
+                if (key > x->keys[i])
+                    ++i;
+            }
+            insert(x->children[i], key);
+        }
+    }
+
+    template <typename T>
+    void BTree<T>::print() const
+    {
+        if (root != nullptr)
+            print(root);
+    }
+
+    template <typename T>
+    void BTree<T>::print(Node<T> *node, int depth) const
+    {
+        if (node != nullptr)
+        {
+            for (int i = 0; i < depth; ++i)
+                std::cout << "  ";
+
+            for (const auto &key : node->keys)
+                std::cout << key << ' ';
+            std::cout << '\n';
+
+            if (!node->isLeaf)
+            {
+                for (const auto &child : node->children)
+                    print(child, depth + 1);
+            }
+        }
     }
 }
--- b-tree/tests/test.cpp
+++ b-tree/tests/test.cpp
@@ -14,4 +14,46 @@ TEST_CASE("B-Tree Test")
     {
         REQUIRE(tree.search(3) == nullptr);
     }
+
+    tree.insert(9);
+    tree.insert(15);
+    tree.insert(17);
+    tree.insert(21);
+    tree.insert(22);
+    tree.insert(23);
+    tree.insert(25);
+    tree.insert(27);
+    tree.insert(30);
+    tree.insert(32);
+    tree.insert(39);
+    tree.insert(40);
+    tree.insert(41);
+    tree.insert(47);
+    tree.insert(55);
+    tree.insert(56);
+    tree.insert(60);
+    tree.insert(63);
+
+    SECTION("Search in B-Tree")
+    {
+        REQUIRE(tree.search(3) == nullptr);
+        REQUIRE(tree.search(9) != nullptr);
+    }
+
+    SECTION("Print B-Tree")
+    {
+        ostringstream capture;
+        streambuf *original = cout.rdbuf(capture.rdbuf());
+        tree.print();
+        cout.rdbuf(original);
+
+        REQUIRE(capture.str() ==
+                "17 23 30 40 55 \n"
+                "  9 15 \n"
+                "  21 22 \n"
+                "  25 27 \n"
+                "  32 39 \n"
+                "  41 47 \n"
+                "  56 60 63 \n");
+    }
 }
